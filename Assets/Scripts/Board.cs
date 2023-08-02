4d75a7bf79e9f18d193a6be9887c7c99c99383fd
Shyplants <wlsdn1004100@naver.com>
2023-08-02 19:36:08 +0900

    MemoryPool 기능 삭제, 수평 움직임 기능 추가
    초기에 n*n 타일 생성 후 (활성화 or 비활성화) 상태 변경 방식으로 수정
    수평 움직임 구현 -> 타일 합치기 기능 구현 필요

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,75 +6,101 @@ public class Board : MonoBehaviour
 {
     [SerializeField]
     private GameObject tilePrafab;
-    private MemoryPool memoryPool;
+
     [SerializeField]
     private Transform tilesParent;
     [SerializeField]
     private float spaceRatio;
     private Dictionary<int, GameObject> boardState;
+    private Dictionary<int, GameObject> nxtBoardState;
     
     private Vector2Int puzzleSize;
     private Vector3 anchorPos;
     private Vector2 tileSize;
     private float Spacing;
 
+    private bool canActive;
+    private float actionDelayTime;
+    private List<Vector2Int> moveList;
+    private bool[] isBlocked;
+    private int moveCount;
+
     private void Awake()
     {
         puzzleSize = new Vector2Int(4, 4);
-        memoryPool = new MemoryPool(tilePrafab, tilesParent, puzzleSize.x);
         boardState = new Dictionary<int, GameObject>();
+        nxtBoardState = new Dictionary<int, GameObject>();
+        moveList = new List<Vector2Int>();
 
         float unitCnt = (puzzleSize.x+1)*spaceRatio + puzzleSize.x;
         RectTransform boarderRectTransform = tilesParent.GetComponent<RectTransform>();
         tileSize = new Vector2(boarderRectTransform.rect.width / unitCnt, boarderRectTransform.rect.height / unitCnt);
         Spacing = tileSize.x * spaceRatio;
         anchorPos = tilesParent.position;
+
+        actionDelayTime = 0.0f;
     }
 
     private void OnApplicationQuit() 
     {
         Debug.Log("Called OnApplicationQuit");
-        memoryPool.DestoryObjects();
     }
 
     private void InitBoard()
     {
         for(int i=0; i<puzzleSize.x*puzzleSize.y; ++i)
         {
-            GameObject clone = memoryPool.ActivePoolItem();
+            GameObject clone = GameObject.Instantiate(tilePrafab, tilesParent);
 
             RectTransform rect = clone.GetComponent<RectTransform>();
             rect.position = GetTargetPos(i);
             rect.sizeDelta = tileSize;
 
             Tile tile = clone.GetComponent<Tile>();
+            tile.Index = i;
             tile.Setup();
+            tile.OnMoveToComplete += HandleMoveToComplete;
 
             boardState[i] = clone;
         }
-
-        memoryPool.DeactivatePoolItems();
     }
 
     private void Start()
     {
         InitBoard();
-
         // test code
         for(int i=0; i<2; ++i)
         {
             SpawnTile();
         }
         
+        canActive = true;
     }
 
     private void Update() 
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
+
+        if(!canActive)
+        {
+            actionDelayTime += Time.deltaTime;
+            if(actionDelayTime > 0.5f)
+            {
+                canActive = true;
+                actionDelayTime = 0.0f;
+            }
+            else
+                return;
+        }
+
         if(x != 0 || y != 0) 
         {
-            MoveTiles(x, y);
+            if(ForceTiles(x, y))
+            {
+                SpawnTile();
+            }
+            canActive = false;
         }
     }
 
@@ -88,79 +114,128 @@ public class Board : MonoBehaviour
         pos += new Vector3(x*tileSize.x + (x+1)*Spacing, -(y*tileSize.y + (y+1)*Spacing), 0);
         return pos;
     }
+
+    private Vector2 GetNextPos(int index)
+    {
+        int y = index / puzzleSize.x;
+        int x = index % puzzleSize.x;
+
+        return new Vector3(x*tileSize.x + (x+1)*Spacing, -(y*tileSize.y + (y+1)*Spacing), 0);
+    }
+
+    
     private void SpawnTile()
     {
         int index;
         while(true)
         {
             index = Random.Range(0, puzzleSize.x*puzzleSize.y);
-            if(!memoryPool.IsActivePoolItem(boardState[index])) break;
+            if(!boardState[index].GetComponent<Tile>().IsActive) break;
         }
 
-        GameObject clone = memoryPool.ActivePoolItem(boardState[index]);
-        Tile tile = clone.GetComponent<Tile>();
+        Tile tile = boardState[index].GetComponent<Tile>();
+        tile.IsActive = true;
+        Debug.Log("Spawned: " + index/puzzleSize.x + " " + index%puzzleSize.x);
         tile.Level = Random.Range(0, 2) + 1;
     }
-    private void MergeTile(int src, int dest)
-    {
-    
-    }
-
     private int Pos2Dto1D(int y, int x)
     {
         return y*puzzleSize.x + x;
     }
 
-    private void MergeTiles(float dx, float dy)
+    private bool OOB(int y, int x)
     {
-        
+        return y < 0 || y >= puzzleSize.y || x < 0 || x >= puzzleSize.x;
     }
-    private void MoveTiles(float dx, float dy)
+
+    private void SwapObject(int src, int dest)
     {
+        GameObject temp = nxtBoardState[src];
+        nxtBoardState[src] = nxtBoardState[dest];
+        nxtBoardState[dest] = temp;
+    }
+
+    private void HandleMoveToComplete()
+    {
+        moveCount--;
+        Debug.Log("이동이 끝났습니다. ");
+
+        if(moveCount == 0)
+        {
+            for(int i=0; i<puzzleSize.x*puzzleSize.y; ++i)
+            {
+                boardState[i] = nxtBoardState[i];
+            }
+            canActive = true;
+        }
+    }
+
+    private bool ForceTiles(float dx, float dy)
+    {
+        bool IsChanged = false;
+        isBlocked = new bool[puzzleSize.x*puzzleSize.y];
+        for(int i=0; i<puzzleSize.x*puzzleSize.y; ++i)
+        {
+            nxtBoardState[i] = boardState[i];
+        }
+        moveCount = 0;
+
         if(dx != 0.0f) // 수평 움직임
         {
+            Debug.Log("수평 움직임");
             bool moveRight = (dx > 0.0f ? true : false);
-            bool IsMerge = false;
+            
             for(int i=0; i<puzzleSize.y; ++i)
             {
-                for(int j=0; j<puzzleSize.x-1; ++j)
+                for(int j=0; j<puzzleSize.x; ++j)
                 {
-                    int cur = (moveRight ? puzzleSize.x-2-j : j+1);
-                    int nxt = (moveRight ? puzzleSize.x-1-j : j);
+                    int cur = (moveRight ? puzzleSize.x-1-j : j);
                     int curIndex = Pos2Dto1D(i, cur);
-                    int nxtIndex = Pos2Dto1D(i, nxt);
+                    int targetIndex = -1;
 
-                    /*
-                    if(boardState[curIndex] > 0)
+                    Tile curTile = boardState[curIndex].GetComponent<Tile>();
+                    if(!curTile.IsActive) continue;
+
+                    int nx = cur;
+                    while(true)
                     {
-                        if(boardState[nxtIndex] == 0) {
-                            // move tile
-                        }
-                        else if(boardState[curIndex] == boardState[nxtIndex])
+                        nx += (moveRight ? 1 : -1);
+                        if(OOB(i, nx)) break;
+
+                        int nextIndex = Pos2Dto1D(i, nx);
+                        Tile nextTile = boardState[nextIndex].GetComponent<Tile>();
+                        if(!nextTile.IsActive) 
                         {
-                            // mergre
-                            memoryPool.DeactivatePoolItem(indexToTile[cur]);
-                            indexToTile.Remove(cur);
-                            boardState[cur] = 0;
-                            
-                            IsMerge = true;
+                            targetIndex = nextIndex;
+                        }
+                        else if(curTile.Level == nextTile.Level)
+                        {
+                            // nextTile 레벨 올려야됨
+                            targetIndex = nextIndex;
+                            isBlocked[nextIndex] = true;
                         }
                     }
 
-                    if((boardState[cur] > 0) && (boardState[cur] == boardState[nxt]))
+                    if(targetIndex != -1)
                     {
-                        
-                        break;
+                        moveCount++;
+                        moveList.Add(new Vector2Int(curIndex, targetIndex));
+                        SwapObject(curIndex, targetIndex);
                     }
-                    */
-                }
+                }                    
             }
-        }
-        else 
-        {
+            if(moveCount == 0) return false;
 
+            canActive = false;
+            foreach(Vector2Int v in moveList)
+            {
+                boardState[v.x].GetComponent<Tile>().OnMoveTo(GetNextPos(v.y));
+            }
+
+            moveList.Clear();
         }
+
+        return IsChanged;
     }
-
-
+    
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,11 +1,20 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
 public class Tile : MonoBehaviour
 {
     private TextMeshProUGUI textNumeric;
+
+    private int index;
+    public int Index
+    {
+        set => index = value;
+        get => index;
+    }
 
     private int numeric;
     public int Numeric
@@ -25,15 +34,33 @@ public class Tile : MonoBehaviour
         {
             level = value;
             Numeric = (1 << level);
+            if(level == 0)
+            {
+                this.gameObject.SetActive(false);
+            }
         }
         get => level;
+    }
+
+    private bool isActive;
+    public bool IsActive
+    {
+        set
+        {
+            isActive = value;
+            this.gameObject.SetActive(isActive);
+        }
+        get => isActive;
     }
 
     public void Setup()
     {
         textNumeric = GetComponentInChildren<TextMeshProUGUI>();
         Level = 0;
+        IsActive = false;
     }
+
+    public event Action OnMoveToComplete;
 
     public void OnMoveTo(Vector3 end)
     {
@@ -44,7 +71,7 @@ public class Tile : MonoBehaviour
     {
         float current = 0;
         float percent = 0;
-        float moveTime = 0.5f;
+        float moveTime = 0.2f;
         Vector3 start = GetComponent<RectTransform>().localPosition;
 
         while(percent < 1.0f)
@@ -56,6 +83,8 @@ public class Tile : MonoBehaviour
 
             yield return null;
         }
+
+        OnMoveToComplete.Invoke();
     }
 
     public bool IsEmpty()
